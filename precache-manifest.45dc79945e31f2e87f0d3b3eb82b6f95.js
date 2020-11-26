61711f25b5393108278987fde354ed9a03108629
bono-slowalk <bono@slowalk.co.kr>
2020-11-27 03:36:01 +0900

    Updates

---
+++ precache-manifest.45dc79945e31f2e87f0d3b3eb82b6f95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74696c1ed300ac776ab0996f89fbd5cc",
+    "url": "/TwoPenceFolio/index.html"
+  },
+  {
+    "revision": "6deb0afd4ebf57e76f6b",
+    "url": "/TwoPenceFolio/static/css/main.0b5a4268.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/TwoPenceFolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/TwoPenceFolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6deb0afd4ebf57e76f6b",
+    "url": "/TwoPenceFolio/static/js/main.b791009b.chunk.js"
+  },
+  {
+    "revision": "ea0599e3ecfb34b4c83a",
+    "url": "/TwoPenceFolio/static/js/runtime-main.877e5482.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/TwoPenceFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/TwoPenceFolio/static/media/your photo.b9d7d448.png"
+  }
+]);
